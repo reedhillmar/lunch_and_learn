0b7932b8f901467bd181bc5aa75cd7dbcf6b303f
Reed Hillmar <reedhillmar@gmail.com>
2023-11-13 10:16:32 -0700

    feat: happy path testing working

--- spec/requests/tourist_sites_request_spec.rb
+++ spec/requests/tourist_sites_request_spec.rb
@@ -5,8 +5,8 @@ describe 'Tourist Sites API' do
     get '/api/v1/tourist_sites?country=france'
 
     response_body = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response).to be be_successful
+require 'pry';binding.pry
+    expect(response).to be_successful
 
     expect(response_body).to have_key :data
     expect(response_body[:data]).to be_an Array
