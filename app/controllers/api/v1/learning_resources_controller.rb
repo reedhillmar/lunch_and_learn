1bb81aa7c04360093685ef9f669ff54917f576cf
Reed Hillmar <reedhillmar@gmail.com>
2023-11-14 10:24:59 -0700

    docs: fleshes out README

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -11,12 +11,4 @@ class Api::V1::LearningResourcesController < ApplicationController
   def make_learning_resources(country)
     LearningResources.new(country)
   end
-
-  def country_video(country)
-    YouTubeService.new.country_video(country)[:items]
-  end
-
-  def country_images(country)
-    PexelsService.new.country_images(country)[:photos]
-  end
 end
